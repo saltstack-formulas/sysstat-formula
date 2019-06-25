84e697a181504b871d3ec5a80dfbceeacac1a3a5
N <noel.mcloughlin@gmail.com>
2019-06-25 16:57:08 +0100

    test(inspec): fix unit test

--- test/integration/default/controls/config_spec.rb
+++ test/integration/default/controls/config_spec.rb
@@ -1,12 +1,14 @@
 control 'sysstat configuration' do
   title 'should match desired lines'
 
-  describe file('/etc/default/sysstat') do
+  describe file('/etc/default/sysstat.ini') do
     it { should be_file }
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'root' }
-    its('mode') { should cmp '0644' }
+    its('mode') { should cmp '0640' }
     its('content') { should include 'Default settings for ' }
     its('content') { should include 'ENABLED' }
+    its('content') { should include 'SA1_OPTIONS="-S DISK"' }
+
   end
 end
